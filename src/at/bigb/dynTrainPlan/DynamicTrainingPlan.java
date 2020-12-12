490a3bee0b88173050822b71943f8b0b030c39c9
BigB <bernhard@boehm-web.com>
2020-12-12 17:44:17 +0100

    BBo DynamicTrainingPlan implementing first simple functions including tests

--- src/at/bigb/dynTrainPlan/DynamicTrainingPlan.java
+++ src/at/bigb/dynTrainPlan/DynamicTrainingPlan.java
@@ -1,22 +1,78 @@
 package at.bigb.dynTrainPlan;
 
+import at.bigb.dynTrainPlan.dao.Round;
+import at.bigb.dynamicTrainingPlan_old.tennis.TennisPlayer;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 /**
- * creates a dynamic training plan depending on start parameters
+ * creates a dynamic training plan for single or double matches depending on start parameters
  */
 public class DynamicTrainingPlan {
 
-    public static void main(String[] args) {
+    private static List<Round> trainingsPlan = new ArrayList<>();
 
+    /**
+     * @param args
+     *   [0] "S"ingle or "D"ouble plan
+     *   [1] Number of courts to fill (only considered with single plans!)
+     *   [2] Number of rounds to play
+     *   [3] Number of valid equal pairings
+     *   [4] Number of valid rounds to pause
+     *   [5] FULL fileName including directory
+     *   [6..n] Player names (use underscore to separate first and last name, use "!" to OPTIONALLY define maximum occurrences afterwards)
+     */
+    public static void main(String[] args) {
+        // first step, check if parameters are valid
+        if (!checkValidDataForPlanCreation(args)) {
+            System.out.println("Error - Invalid number/type of arguments!");
+            return;
+        }
+        String fileName = args[5];
+
+        // last step, create the trainingsPlan file
+        buildAndWriteCsvFile(fileName);
     }
 
-    public boolean checkValidDataForPlanCreation(String[] args) {
-        if (args == null || args.length < 3) {
+    public static boolean checkValidDataForPlanCreation(String[] args) {
+        // check correct amount of parameters and eventually types also
+        if (args == null || args.length < 8) {
             return false;
         }
         return true;
     }
 
-    public void buildAndWriteCsvFile() {
+    public static void buildAndWriteCsvFile(String fileName) {
+        try {
+            BufferedWriter writer = new BufferedWriter
+                    (new OutputStreamWriter(new FileOutputStream(fileName),"UTF-8"));
+            // loop over trainingPlan (and write line for each round)
+            for(int roundNo = 1; roundNo <= trainingsPlan.size(); roundNo++) {
+                writer.append(Integer.toString(roundNo));
+                writer.append(',');
 
+                // new line
+                writer.append('\n');
+            }
+            // write the data to the file
+            writer.flush();
+            writer.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<Round> getTrainingsPlan() {
+        return trainingsPlan;
+    }
+
+    public void setTrainingsPlan(List<Round> trainingsPlan) {
+        this.trainingsPlan = trainingsPlan;
     }
 }
--- src/at/bigb/dynTrainPlan/DynamicTrainingPlanTest.java
+++ src/at/bigb/dynTrainPlan/DynamicTrainingPlanTest.java
@@ -1,7 +1,16 @@
 package at.bigb.dynTrainPlan;
 
+import at.bigb.dynTrainPlan.dao.Player;
+import at.bigb.dynTrainPlan.dao.Round;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,5 +29,45 @@ class DynamicTrainingPlanTest {
         assertFalse(trainingsPlan.checkValidDataForPlanCreation(args));
         args = new String[] {"D", "2", "BigB"};
         assertTrue(trainingsPlan.checkValidDataForPlanCreation(args));
+    }
+
+    @Test
+    void checkCsvFileCreation() {
+        String testFileName = "C://_boehm//bbo_dynamicPlanGeneration_test.csv";
+        // first delete possible file
+        File deleteFile = new File(testFileName);
+        deleteFile.delete();
+        // now check file creation
+        this.createTrainingsPlanEntries();
+        trainingsPlan.buildAndWriteCsvFile(testFileName);
+        boolean fileExists = true;
+        FileReader file = null;
+        try {
+            file = new FileReader(testFileName);
+        } catch (FileNotFoundException e) {
+            fileExists = false;
+        }
+        assertTrue(fileExists);
+    }
+
+    private void createTrainingsPlanEntries() {
+        List<Round> entries = new ArrayList<>();
+        for(int i=0;i < 26;i++) {
+            Round round = new Round();
+            Player player1 = new Player("Player1");
+            Player player2 = new Player("Player2");
+            Player player3 = new Player("Player3");
+            Player player4 = new Player("Player4");
+            round.setPlayers(Arrays.asList(player1,player2,player3,player4));
+            entries.add(round);
+        }
+        // set entries in trainingsPlan
+        trainingsPlan.setTrainingsPlan(entries);
+    }
+
+    @Test
+    void validTrainingsPlanEntriesCreation() {
+        this.createTrainingsPlanEntries();
+        assertTrue(trainingsPlan.getTrainingsPlan().size() == 26);
     }
 }
